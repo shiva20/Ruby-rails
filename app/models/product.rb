970550bdd0f15e7b591b1e10023c909d6207a0c7
shiva Ruby <shivaprasad.kola@gmail.com>
2014-11-28 17:45:48 +0530

    cached new product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,9 @@ class Product < ActiveRecord::Base
   validates :price, numericality: {greater_than_or_equal_to: 0.01} 
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i,message: 'must be a URL for GIF, JPG or PNG image.'}
+  
+  def self.latest
+    Product.order(:updated_at).last
+  end
+  
 end
