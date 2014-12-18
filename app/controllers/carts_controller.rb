39dd42e17a53b39af8a1d8e4ffbe387735143c86
shiva Ruby <shivaprasad.kola@gmail.com>
2014-12-18 15:08:01 +0530

    Added smarter cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
-
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   # GET /carts
   # GET /carts.json
   def index
@@ -21,6 +21,10 @@ class CartsController < ApplicationController
   def edit
   end
 
+  def invalid_cart
+  logger.error "Attempt to access invalid cart #{params[:id]}" 
+  redirect_to store_url, notice: 'Invalid cart'
+  end
   # POST /carts
   # POST /carts.json
   def create
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -29,7 +29,7 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-     @line_item = @cart.line_items.build(Product: product)
+     @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
